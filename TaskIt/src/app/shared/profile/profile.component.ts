e1cd5cb83535ddffd1edee2ad55b1c4bddf6b69f
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2023-11-18 01:13:46 -0600

    tweaking profile block

--- TaskIt/src/app/shared/profile/profile.component.ts
+++ TaskIt/src/app/shared/profile/profile.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { User } from '../auth/user.model'
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit, OnDestroy {
   // myProfile: User[] =
   //   [new User ('Aaron Whitmer','myemail@myemail.com','https://source.unsplash.com/50x50/?scenery')];
 
+  authUser: Subscription;
+  currentUser: User;
 
+  constructor(private authsvc: AuthService){}
+
+  ngOnInit(): void {
+   this.authUser = this.authsvc.currentUser.subscribe(data => {
+    this.currentUser = data;
+    console.log("Current User: ", this.currentUser)
+   })
+  }
+
+  ngOnDestroy(): void {
+  this.authUser.unsubscribe();
+  }
 }
 
 
